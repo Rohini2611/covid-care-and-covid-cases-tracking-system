33eb72c6a59960232101581f17951dd35f05fcae
Rohini Jadhav <rohinijadhav2611@gmail.com>
2021-12-09 23:47:09 +0530

    first commit

---
+++ src/Sections/SpreadOfCovid.js
@@ -0,0 +1,49 @@
+import Cart from "../Components/Cart";
+
+import coughing from "../images/coughing.svg";
+import hand_shake from "../images/hand_shake.svg";
+import touching_surface from "../images/touching_surface.svg";
+
+const SpreadOfCovid = () => {
+  return (
+    <Cart>
+      <section id="sect-awareness">
+        <h2>HOW DOES COVID-19 SPREAD?</h2>
+        <div id="cards-container-awareness">
+          <div class="card">
+            <img src={coughing} class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h4>Coughing/Sneezing</h4>
+              <p class="card-text">
+                The virus is transmitted through direct contact with respiratory
+                droplets of an infected person
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <img src={hand_shake} class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h4>Close Contact</h4>
+              <p class="card-text">
+                Direct skin-to-skin contact and then touching face eg.eyes, nose
+                increases likelihood of infection
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <img src={touching_surface} class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h4>Touching Infected Surfaces</h4>
+              <p class="card-text">
+                Individuals can also be infected from and touching surfaces
+                contaminated with the virus
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Cart>
+  );
+};
+
+export default SpreadOfCovid;
